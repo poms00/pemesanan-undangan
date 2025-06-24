da593369435d8d0445f065a661b28e74c9af6982
aditya <herlambangaditya27@gmail.com>
2025-06-25 04:51:11 +0700

    test: simple route for Railway

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::view('/', 'welcome');
+Route::get('/', fn() => 'Laravel is working!');
+
 
 Route::get('/login', \App\Livewire\Auth\Login::class);
 
@@ -12,5 +13,4 @@ Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
-require __DIR__.'/auth.php';
-
+require __DIR__ . '/auth.php';
