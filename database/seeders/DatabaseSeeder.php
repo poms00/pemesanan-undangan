ebd2a2d113eda944b9fdee39ea586e94bbe9fbce
aditya <herlambangaditya27@gmail.com>
2025-06-29 04:30:00 +0700

    Ubah seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,28 +16,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        //  User::factory(2)->create();
-        //  User::create([
-        //      'name' => 'Admin',
-        //            'email' => 'admin@example.com',
-        //      'password' => Hash::make('12345678'), // tetap harus di-hash
-        //     'role' => 'admin',
-        //    'email_verified_at' => now(),
-        // ]);
+          User::factory(2)->create();
+          User::create([
+        'name' => 'Admin',
+                    'email' => 'admin@example.com',
+             'password' => Hash::make('12345678'), // tetap harus di-hash
+            'role' => 'admin',
+           'email_verified_at' => now(),
+         ]);
 
-        //  User::create([
-        //      'name' => 'User Biasa',
-        //    'email' => 'user@example.com',
-        //   'password' => Hash::make('12345678'),
-        //   'role' => 'user',
-        //   'email_verified_at' => now(),
-        // ]);
+         User::create([
+             'name' => 'User Biasa',
+           'email' => 'user@example.com',
+          'password' => Hash::make('12345678'),
+          'role' => 'user',
+           'email_verified_at' => now(),
+         ]);
 
 
         KategoriProduk::factory()->count(3)->create();
 
 
-        //Produk::factory()->count(5)->create();
+        Produk::factory()->count(20)->create();
 
 
     }
